63f38549702bafc4bebc6cfa624602bdc2e972cb
Vasiliy Kylik <detroit.vv@gmail.com>
2018-08-26 20:57:19 +0300

    InjectRandomIntAnnotationBeanPostProcessor

---
+++ src/main/java/quoters/InjectRandomInt.java
@@ -0,0 +1,13 @@
+package quoters;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 26.08.2018.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectRandomInt {
+    int min();
+    int max();
+}
--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -4,6 +4,9 @@ package quoters;
  * Created by Vasiliy Kylik (Lightning) on 26.08.2018.
  */
 public class TerminatorQuoter implements Quoter {
+
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
     private String message;
 
     public void setMessage(String message) {
@@ -12,6 +15,7 @@ public class TerminatorQuoter implements Quoter {
 
     @Override
     public void sayQuote() {
-        System.out.println("message = " + message);
+        for (int i = 0; i < repeat; i++)
+            System.out.println("message = " + message);
     }
 }
