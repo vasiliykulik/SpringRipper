7916adc7d5bf9606a0f4355de34be6373eee1513
Vasiliy Kylik <detroit.vv@gmail.com>
2018-08-26 22:01:11 +0300

    Two-Phase Constructor

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,5 +1,7 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Created by Vasiliy Kylik (Lightning) on 26.08.2018.
  */
@@ -8,6 +10,16 @@ public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
     private String message;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     public void setMessage(String message) {
         this.message = message;
