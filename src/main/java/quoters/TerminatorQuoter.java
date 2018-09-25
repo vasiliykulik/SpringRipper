9e6b2d0107f72866d50a12bf74c3fc1f13ba0cec
Vasiliy Kylik <detroit.vv@gmail.com>
2018-09-25 21:02:20 +0300

    PropertyFileApplicationContext

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -8,6 +8,10 @@ import javax.annotation.PostConstruct;
 @Profiling
 @DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
+    // И даже для context.properties нужен сеттер
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
+    }
 
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
